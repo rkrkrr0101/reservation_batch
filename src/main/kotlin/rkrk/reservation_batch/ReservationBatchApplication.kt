99f2aa5e0f5cd135db21795cc763a58f103a7f41
foko <qkrrudgh0@naver.com>
2024-11-26 00:04:53 +0900

    init

---
+++ src/main/kotlin/rkrk/reservation_batch/ReservationBatchApplication.kt
@@ -0,0 +1,11 @@
+package rkrk.reservation_batch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReservationBatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReservationBatchApplication>(*args)
+}
---
+++ src/test/kotlin/rkrk/reservation_batch/ReservationBatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package rkrk.reservation_batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReservationBatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
